2d99ce135abd5f2a9d765bacc8fd9a3898dcf0ff
emodan100 <71289072+emodan100@users.noreply.github.com>
2025-12-02 22:02:17 +0300

    1 Bölüm değişkenler ve hazırladığım basit uygulama
    Uygulama içinde mini karakter oluşturucu değişkenlerle tasarlanmış ve mini market hesaplayıcı var oda değişkenlerle tasarlanmış.

---
+++ Degiskenler1(MkveMm).cs
@@ -0,0 +1,23 @@
+﻿//Mini Karakter Oluşturucu
+
+var Karakter = "Arin";
+var Irk = "Elf";
+var Sınıf = "Okçu";
+var Seviye = 5;
+var Sağlık = 100;
+
+Console.WriteLine("Karakterinin Adı: " + Karakter);
+Console.WriteLine("Karakterinin Irkı: " + Irk);
+Console.WriteLine("Karakterinin Sınıfı: " + Sınıf);
+Console.WriteLine("Karakterinin Seviyesi: " + Seviye);
+Console.WriteLine("Karakterinin Sağlığı: " + Sağlık);
+
+//Mini Market Hesaplayıcı
+
+var Urun1 = 16;
+var Urun2 = 25.50;
+var Urun3 = 32.75;
+
+var ToplamTutar = Urun1 + Urun2 + Urun3;
+
+Console.WriteLine("Alışveriş Sepetinizin Toplam Tutarı: $" + ToplamTutar);
