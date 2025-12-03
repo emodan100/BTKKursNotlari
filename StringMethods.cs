f32cf051635f4e27c91a3a6300bbf79bf7db451b
emodan100 <71289072+emodan100@users.noreply.github.com>
2025-12-03 20:46:43 +0300

    String Methodları
    String methodları ile yaptığım işlemler ve string interpolation

---
+++ StringMethods.cs
@@ -0,0 +1,28 @@
+﻿string deneme = "Oyuncunuzun toplam lvl 12";
+
+//Sahip Karakter
+int Karakter = deneme.Length;
+Console.WriteLine("Deneme karakter sayısı: " + Karakter);
+
+//Hepsini küçük harf yapma 
+string kucuk = deneme.ToLower();
+Console.WriteLine(kucuk);
+
+//Karakter sorgulama
+bool varmi = deneme.StartsWith("O");
+Console.WriteLine(varmi);
+
+//Replace atma
+string Replace = deneme.Replace("lvl", "level");
+Console.WriteLine(Replace);
+
+//Karakter konumu bulma
+var konum = deneme.IndexOf("toplam");
+Console.WriteLine(konum);
+
+//String interpolation
+Console.Write("Karakterinizin leveli: ");
+string? level = Console.ReadLine();
+Console.Write("Karakterinizin ismi: ");
+string? isim = Console.ReadLine();
+Console.WriteLine($"Karakter ismi: {isim}, Karakter leveli: {level}");
